c999d69adac0e2a132c8ff9d28a6b35ba6e6e843
root <root@DESKTOP-9V70QIK>
2023-06-05 07:01:59 +0300

    atoi changed

--- ft_atoi.c
+++ ft_atoi.c
@@ -2,29 +2,20 @@
 
 int ft_atoi(char *c)
 {
-    static unsigned short int i;
-    static unsigned short int sign;
     int result;
-
-    sign = 1;
+    int i; 
     result = 0;
-
-
+    i = 0;
 
     while (c[i] != '\0')
     {
-        if (c[i] == '-' || c[i] == '+')
-        {   
-            if (c[i] == '-')
-                sign *= -1;
-        }
-
         if ('0' <= c[i] && c[i] <= '9')
         {
-            result += (c[i] + '0')  * 10;
+            result += (c[i] - '0');
+            result *= 10;
         }
         i++;
     }
 
-    return result * sign; 
+    return result / 10; 
 }
